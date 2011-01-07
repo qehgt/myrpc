4a098e5c92de1692641faceefbb8cf1438f20f96
Vasily Titskiy <qehgt0@gmail.com>
2011-01-07 14:13:16 -0500

    Remove file

--- myrpc/stdafx.h
+++ myrpc/stdafx.h
@@ -5,14 +5,12 @@
 
 #pragma once
 
-#include "targetver.h"
+#if defined(_WIN32) && !defined(_WIN32_WINNT)            // Specifies that the minimum required platform is Windows Vista.
+#  define _WIN32_WINNT 0x0600     // Change this to the appropriate value to target other versions of Windows.
+#endif
+
 
 #include <stdio.h>
-#include <tchar.h>
-
-
-
-// TODO: reference additional headers your program requires here
 #include <map>
 #include <boost/weak_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
