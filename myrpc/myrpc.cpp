48e9850a323de7e4e4070799843aa4982a8a455d
Vasily Titskiy <qehgt0@gmail.com>
2011-01-10 13:34:57 -0500

    Use asio::io_service

--- myrpc/myrpc.cpp
+++ myrpc/myrpc.cpp
@@ -4,7 +4,6 @@
 #include "stdafx.h"
 
 class session;
-class my_socket {};
 struct data_type {
     int i;
     float f;
@@ -34,7 +33,45 @@ protected:
 
 class session : public boost::enable_shared_from_this<session> {
 public:
-    session() : current_id(0) {}
+    session(boost::asio::io_service& io_service)
+        : socket(io_service)
+    {}
+
+    boost::asio::ip::tcp::socket& get_socket()
+    {
+        return socket;
+    }
+
+    void start()
+    {
+        socket.async_read_some(boost::asio::buffer(buff, max_length),
+            boost::bind(&session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+
+    void handle_read(const boost::system::error_code& error,
+        size_t bytes_transferred)
+    {
+        if (!error)
+        {
+            // process input data...
+
+            /*
+            boost::asio::async_write(socket,
+                boost::asio::buffer(data, bytes_transferred),
+                boost::bind(&session::handle_write, this,
+                boost::asio::placeholders::error));
+                */
+        }
+        else
+        {
+            socket.shutdown(boost::asio::socket_base::shutdown_both);
+            socket.close();
+        }
+    }
+
+
 
     callable create_call();
     void remove_unused_callable(session_id_type id, bool reset_data);
@@ -43,11 +80,15 @@ protected:
     session_id_type current_id;
     typedef boost::shared_ptr<boost::promise<data_type> > promise_type;
     typedef std::map<session_id_type, promise_type> promise_map_type;
-    my_socket s;
 
     typedef boost::recursive_mutex mutex_type;
     mutex_type mutex;
     promise_map_type promise_map;
+
+
+    enum { max_length = 1024 };
+    char buff[max_length];
+    boost::asio::ip::tcp::socket socket;
 };
 
 callable::~callable()
@@ -84,14 +125,26 @@ callable session::create_call()
 
 int main()
 {
-    boost::asio::io_service io;
-    boost::thread t(boost::bind(&boost::asio::io_service::run, &io));
+    using namespace boost::asio;
+    const int PORT = 18811;
+    io_service io;
 
-    boost::shared_ptr<session> s(new session);
+    ip::tcp::acceptor acceptor(io, ip::tcp::endpoint(ip::tcp::v4(), PORT));
+
+    boost::shared_ptr<session> s(new session(io));
+    acceptor.accept(s->get_socket());
+    s->start();
+    boost::thread t(boost::bind(&io_service::run, &io));
+
+
+    /*
     callable call = s->create_call();
     future_data& f = call.get_future();
     // f.get(); // wait until result
+    */
 
+    
+    //boost::this_thread::sleep(boost::posix_time::seconds(1000));
     t.join();
     return 0;
 }
