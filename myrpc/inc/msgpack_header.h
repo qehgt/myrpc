49e0d460e78379d6ead431e8eaf13e50cd00eafe
Vasily Titskiy <qehgt0@gmail.com>
2011-01-14 13:51:43 -0500

    Warnings hunt

---
+++ myrpc/inc/msgpack_header.h
@@ -0,0 +1,15 @@
+#ifndef H_MYRPC_MSGPACK_HEADER_H
+#define H_MYRPC_MSGPACK_HEADER_H
+
+#if defined(_MSC_VER)
+#  pragma warning(push)
+#  pragma warning(disable:4244)
+#endif // _MSC_VER
+
+#include <msgpack.hpp>
+
+#if defined(_MSC_VER)
+#  pragma warning(pop)
+#endif // _MSC_VER
+
+#endif // H_MYRPC_MSGPACK_HEADER_H
