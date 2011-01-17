dd39afad70a8ae50279c071b21b7e93241aabe07
Vasily Titskiy <qehgt0@gmail.com>
2011-01-17 13:25:37 -0500

    Add interface file
    --HG--
    branch : hide_imp

---
+++ myrpc/inc/io_stream_object.h
@@ -0,0 +1,24 @@
+#ifndef H_MYRPC_IO_STREAM_OBJECT_H
+#define H_MYRPC_IO_STREAM_OBJECT_H
+
+#include <stdlib.h>
+#include <boost/system/error_code.hpp>
+
+namespace msgpack {
+namespace myrpc {
+
+class handle_reader_type {
+    virtual void handle_read(const boost::system::error_code& error, size_t bytes_transferred) = 0;
+}
+
+class io_stream_object {
+    virtual ~io_stream_object() {}
+
+    virtual size_t write(const void* data, size_t size, boost::system::error_code& ec);
+    virtual void async_read_some(void* data, size_t size, handle_reader_type* handler);
+};
+
+} // namespace myrpc {
+} // namespace msgpack {
+
+#endif // H_MYRPC_IO_STREAM_OBJECT_H
