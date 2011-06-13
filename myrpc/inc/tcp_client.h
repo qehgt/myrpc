b2869e6804a6bcf493245e92e288e90bfced67e0
Vasily Titskiy <qehgt0@gmail.com>
2011-06-13 16:07:07 -0400

    New version of TCP client
    --HG--
    branch : client_refactoring

---
+++ myrpc/inc/tcp_client.h
@@ -0,0 +1,23 @@
+#ifndef H_MYRPC_TCP_CLIENT_H
+#define H_MYRPC_TCP_CLIENT_H
+
+#include "rpc_client.h"
+
+namespace msgpack {
+namespace myrpc {
+
+class tcp_client : public rpc_client {
+  public:
+    tcp_client(const char* host, const char* service_name, shared_dispatcher dispatcher = shared_dispatcher());
+    ~tcp_client();
+    
+  protected:
+    struct tcp_client_impl;
+    boost::shared_ptr<tcp_client_impl> pimpl;
+};
+
+}  // namespace myrpc
+}  // namespace msgpack
+
+#endif // H_MYRPC_TCP_CLIENT_H
+
