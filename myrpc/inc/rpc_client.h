558031c0141f995e12b06b5ca9ec2f0ec81ea903
Vasily Titskiy <qehgt0@gmail.com>
2011-06-13 15:58:47 -0400

    Rename files
    --HG--
    branch : client_refactoring
    rename : myrpc/inc/tcp_client.h => myrpc/inc/rpc_client.h
    rename : myrpc/inc/tcp_client.hmpl => myrpc/inc/rpc_client.hmpl

--- myrpc/inc/rpc_client.h
+++ myrpc/inc/rpc_client.h
@@ -1,5 +1,5 @@
-#ifndef H_MYRPC_TCP_CLIENT_H
-#define H_MYRPC_TCP_CLIENT_H
+#ifndef H_MYRPC_RPC_CLIENT_H
+#define H_MYRPC_RPC_CLIENT_H
 
 #include <boost/shared_ptr.hpp>
 #include "session.h"
@@ -7,10 +7,10 @@
 namespace msgpack {
 namespace myrpc {
 
-class tcp_client {
+class rpc_client {
 public:
-    tcp_client(const char* host, const char* service_name, shared_dispatcher dispatcher = shared_dispatcher());
-    ~tcp_client();
+    virtual ~rpc_client()
+    {}
 
     inline callable call(const std::string& name);
 
@@ -90,143 +90,141 @@ public:
 
 
 protected:
-    struct tcp_client_impl;
-    boost::shared_ptr<tcp_client_impl> pimpl;
     boost::shared_ptr<myrpc::session> session;
 };
 
 
-inline callable tcp_client::call(const std::string& name)
+inline callable rpc_client::call(const std::string& name)
 {
     return session->call(name);
 }
 
-inline void tcp_client::notify(const std::string& name)
+inline void rpc_client::notify(const std::string& name)
 {
     session->notify(name);
 }
 
 template <typename A1>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1)
 {
     return session->call(name, a1);
 }
 
 template <typename A1>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1)
 {
     session->notify(name, a1);
 }
 
 template <typename A1, typename A2>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2)
 {
     return session->call(name, a1, a2);
 }
 
 template <typename A1, typename A2>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2)
 {
     session->notify(name, a1, a2);
 }
 
 template <typename A1, typename A2, typename A3>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3)
 {
     return session->call(name, a1, a2, a3);
 }
 
 template <typename A1, typename A2, typename A3>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3)
 {
     session->notify(name, a1, a2, a3);
 }
 
 template <typename A1, typename A2, typename A3, typename A4>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4)
 {
     return session->call(name, a1, a2, a3, a4);
 }
 
 template <typename A1, typename A2, typename A3, typename A4>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4)
 {
     session->notify(name, a1, a2, a3, a4);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5)
 {
     return session->call(name, a1, a2, a3, a4, a5);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5)
 {
     session->notify(name, a1, a2, a3, a4, a5);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6)
 {
     return session->call(name, a1, a2, a3, a4, a5, a6);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6)
 {
     session->notify(name, a1, a2, a3, a4, a5, a6);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7)
 {
     return session->call(name, a1, a2, a3, a4, a5, a6, a7);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7)
 {
     session->notify(name, a1, a2, a3, a4, a5, a6, a7);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8)
 {
     return session->call(name, a1, a2, a3, a4, a5, a6, a7, a8);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8)
 {
     session->notify(name, a1, a2, a3, a4, a5, a6, a7, a8);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8, typename A9>
-inline callable tcp_client::call(const std::string& name,
+inline callable rpc_client::call(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9)
 {
     return session->call(name, a1, a2, a3, a4, a5, a6, a7, a8, a9);
 }
 
 template <typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7, typename A8, typename A9>
-inline void tcp_client::notify(const std::string& name,
+inline void rpc_client::notify(const std::string& name,
             const A1& a1, const A2& a2, const A3& a3, const A4& a4, const A5& a5, const A6& a6, const A7& a7, const A8& a8, const A9& a9)
 {
     session->notify(name, a1, a2, a3, a4, a5, a6, a7, a8, a9);
@@ -236,4 +234,4 @@ inline void tcp_client::notify(const std::string& name,
 }  // namespace myrpc
 }  // namespace msgpack
 
-#endif // H_MYRPC_TCP_CLIENT_H
+#endif // H_MYRPC_RPC_CLIENT_H
