043eca6c24d14eab8db6f97ab492e26d0efe6d45
Vasily Titskiy <qehgt0@gmail.com>
2012-01-22 18:13:44 +0000

    Do not use boost's atomic operations if GCC has correspond buildins

--- myrpc/inc/atomic_ops.h
+++ myrpc/inc/atomic_ops.h
@@ -5,6 +5,8 @@
 
 #if defined(__GNUC__) && ((__GNUC__*10 + __GNUC_MINOR__) < 41)
 #  include <bits/atomicity.h>
+#elif defined(__GNUC__) && ((__GNUC__*10 + __GNUC_MINOR__) >= 41)
+# // use GCC builtins 
 #else
 #  include <boost/cstdint.hpp>
 #  include <boost/interprocess/detail/atomic.hpp>
@@ -21,6 +23,16 @@ static inline
 atomic_int_type atomic_increment(volatile atomic_int_type* p)
 {
   return  __gnu_cxx::__exchange_and_add(p, 1);
+}
+
+#elif defined(__GNUC__) && ((__GNUC__*10 + __GNUC_MINOR__) >= 41)
+
+typedef int atomic_int_type;
+
+static inline
+atomic_int_type atomic_increment(volatile atomic_int_type* p)
+{
+  return __sync_fetch_and_add(p, 1);
 }
 
 #else
