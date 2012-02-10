beb1f2b6690f73ba8d8c9b8912bff850d24e5093
Damian <noname@noname.com>
2012-02-10 17:19:11 +0000

    add unit tests, using gtest testing framework

---
+++ myrpc/target/myrpc_unittest/myecho_server.h
@@ -0,0 +1,72 @@
+#ifndef H_MYECHO_SERVER_H
+#define H_MYECHO_SERVER_H
+
+#include "inc/interfaces.h"
+
+class myecho : public msgpack::myrpc::dispatcher_type {
+public:
+    typedef msgpack::myrpc::request request;
+
+	void dispatch(request req)
+    {
+        try {
+            std::string method;
+            req.method().convert(&method);
+
+            if (method == "add") {
+                msgpack::type::tuple<int, int> params;
+                req.params().convert(&params);
+                add(req, params.get<0>(), params.get<1>());
+
+            } else if (method == "echo") {
+                msgpack::type::tuple<std::string> params;
+                req.params().convert(&params);
+                echo(req, params.get<0>());
+
+            } else if (method == "echo_huge") {
+                msgpack::type::tuple<msgpack::type::raw_ref> params;
+                req.params().convert(&params);
+                echo_huge(req, params.get<0>());
+
+            } else if (method == "err") {
+                msgpack::type::tuple<> params;
+                req.params().convert(&params);
+                err(req);
+
+            } else {
+                req.error(msgpack::myrpc::NO_METHOD_ERROR);
+            }
+
+        } catch (msgpack::type_error&) {
+            req.error(msgpack::myrpc::ARGUMENT_ERROR);
+            return;
+
+        } catch (std::exception& e) {
+            req.error(std::string(e.what()));
+            return;
+        }
+    }
+
+
+	void add(request req, int a1, int a2)
+	{
+		req.result(a1 + a2);
+	}
+
+	void echo(request req, const std::string& msg)
+	{
+		req.result(msg);
+	}
+
+	void echo_huge(request req, const msgpack::type::raw_ref& msg)
+	{
+		req.result(msg);
+	}
+
+	void err(request req)
+	{
+		req.error(std::string("always fail"));
+	}
+};
+
+#endif //  H_MYECHO_SERVER_H
---
+++ myrpc/target/myrpc_unittest/myrpc_unittest.cpp
@@ -0,0 +1,210 @@
+#include <gtest/gtest.h>
+
+#include "myecho_server.h"
+#include "inc/stream_tcp_socket.h"
+#include "inc/session.h"
+#include "inc/tcp_client.h"
+#include "inc/tcp_server.h"
+#include "inc/exception.h"
+#include <boost/make_shared.hpp>
+
+GTEST_API_ int main(int argc, char **argv)
+{
+	printf("Running main() from myrpc_unittest.cc\n");
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+TEST(EchoServer, Add)
+{
+	using namespace boost;
+	using namespace boost::asio;
+    using namespace boost::asio::ip;
+    using namespace msgpack;
+    using namespace msgpack::myrpc;
+    const char* port = "18811";
+
+	try {
+		const int PORT = 18811;
+        tcp_server server(PORT, make_shared<myecho>());
+	
+		const char* port = "18811";
+		tcp_client cli("127.0.0.1", port);
+
+        int a = -12;
+        int b = 13;
+        callable cc = cli.call("add", a, b);
+        int i = cc.get<int>();
+		EXPECT_EQ((a+b), i);
+
+	} catch(const argument_error& e) {
+		ADD_FAILURE() << "argument_error exception " << e.what();
+	}
+	catch (const system::system_error& e)
+	{
+		ADD_FAILURE() << e.what();
+	}	
+	catch (...)
+	{
+		ADD_FAILURE() << "Uncaught exception";
+	}
+}
+
+
+TEST(EchoServer, IncorrectPort)
+{
+	using namespace msgpack;
+    using namespace msgpack::myrpc;
+    using namespace boost;
+
+	try {
+        const int PORT = 18811;
+        tcp_server server(PORT, make_shared<myecho>());       
+
+    	tcp_client cli("127.0.0.1", "16296");
+		cli.call("add", 1, 2).get<int>();
+	} catch(const argument_error& e) {
+		ADD_FAILURE() << "argument_error exception " << e.what();
+	}
+	catch (const system::system_error&)
+	{
+		SUCCEED();
+		return;
+	}	
+	catch (...)
+	{
+		FAIL() << "Uncaught exception";
+	}
+	ADD_FAILURE() << "Didn't throw exception as expected";
+}
+
+TEST(EchoServer, NoMethodError)
+{
+	using namespace msgpack;
+    using namespace msgpack::myrpc;
+    using namespace boost;
+
+	try {
+        const int PORT = 18811;
+        tcp_server server(PORT, make_shared<myecho>());       
+   
+		tcp_client cli("127.0.0.1", "18811");
+		cli.call("sub", 1, 2).get<int>();
+	} catch(const argument_error& e) {
+		ADD_FAILURE() << "argument_error exception " << e.what();
+	}	
+	catch (const std::exception&)
+	{
+		SUCCEED();
+		return;
+	}
+	catch (...)
+	{
+		FAIL() << "Uncaught exception";
+	}
+	ADD_FAILURE() << "Didn't throw exception as expected";
+}
+
+TEST(EchoServer, Err)
+{
+	using namespace msgpack;
+    using namespace msgpack::myrpc;
+    using namespace boost;
+
+	try {
+        const int PORT = 18811;
+        tcp_server server(PORT, make_shared<myecho>());       
+   
+		tcp_client cli("127.0.0.1", "18811");
+		cli.call("err").get<int>();
+	} 			
+	catch (const std::exception&)
+    {
+		SUCCEED();
+        return;
+    }
+	catch (...)
+	{
+		FAIL() << "Uncaught exception";
+		return;
+	}
+	ADD_FAILURE() << "Didn't throw exception as expected";
+}
+
+
+TEST(EchoServer, Notify)
+{
+	using namespace msgpack;
+    using namespace msgpack::myrpc;
+    using namespace boost;
+
+	try {
+        const int PORT = 18811;
+        tcp_server server(PORT, make_shared<myecho>());       
+   
+		tcp_client cli("127.0.0.1", "18811");
+		cli.notify("echo");
+	} catch(const argument_error& e) {
+		ADD_FAILURE() << "argument_error exception " << e.what();
+	}	
+	catch (const system::system_error& e)
+	{
+		ADD_FAILURE() << "argument_error exception " << e.what();
+	}
+	catch (...)
+	{
+		FAIL() << "Uncaught exception";
+	}	
+}
+
+
+
+// #include "attack.h"
+// #include <cclog/cclog.h>
+// #include <cclog/cclog_tty.h
+
+/*
+static size_t ATTACK_THREAD;
+static size_t ATTACK_LOOP;
+
+static std::auto_ptr<attacker> test;
+
+void attack_connect()
+{
+	for(size_t i=0; i < ATTACK_LOOP; ++i) {
+		rpc::client c(test->builder(), test->address());
+		c.set_timeout(30.0);
+
+		int result = c.call("add", 1, 2).get<int>();
+		if(result != 3) {
+			// LOG_ERROR("invalid response: ",result);
+			std::cout << "invalid response: " << result << std::endl;
+		}
+	}
+}
+
+int AttackConnect()
+{
+	// cclog::reset(new cclog_tty(cclog::WARN, std::cout));
+	signal(SIGPIPE, SIG_IGN);
+
+	ATTACK_THREAD = attacker::option("THREAD", 25, 100);
+	ATTACK_LOOP   = attacker::option("LOOP", 5, 50);
+
+	std::cout << "connect attack"
+		<< " thread=" << ATTACK_THREAD
+		<< " loop="   << ATTACK_LOOP
+		<< std::endl;
+
+	test.reset(new attacker());
+	test->run(ATTACK_THREAD, &attack_connect);
+	
+	return 0;
+}
+
+// Tests a heavy attack on the server
+TEST(HeavyAttack, ConnectTest) {
+  // This tests the connection between client and server
+  EXPECT_EQ(0, AttackConnect());
+}
+*/
